84471a530c6d01b3df4fc132d3103cb4d5d39786
mcalisadeghi <ali.ke970@gmail.com>
2023-11-15 00:47:58 +0330

    buildAnimatedContainer

--- lib/screen/my_services.dart
+++ lib/screen/my_services.dart
@@ -13,6 +13,11 @@ class MyServices extends StatelessWidget {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.symmetric(
+        vertical: 30.0,
+        horizontal: 12.0,
+      ),
       width: size.width,
       height: size.height,
       color: AppColors.bgColor,
@@ -41,46 +46,97 @@ class MyServices extends StatelessWidget {
             ),
           ),
           Constants.sizedBox(
-            height: 40.0,
+            height: 60.0,
           ),
-          AnimatedContainer(
-            duration: Duration(
-              microseconds: 600,
+          Row(
+            children: [
+              buildAnimatedContainer(
+                title: 'App Development',
+                asset: AppAssets.github,
+              ),
+              Constants.sizedBox(
+                width: 18.0,
+              ),
+              buildAnimatedContainer(
+                title: 'Graphic Designing',
+                asset: AppAssets.instagram,
+              ),
+              Constants.sizedBox(
+                width: 18.0,
+              ),
+              buildAnimatedContainer(
+                title: 'Digital Marketing',
+                asset: AppAssets.facebook,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  AnimatedContainer buildAnimatedContainer({
+    required String title,
+    required String asset,
+  }) {
+    return AnimatedContainer(
+      duration: const Duration(
+        microseconds: 600,
+      ),
+      width: 400.0,
+      height: 450.0,
+      alignment: Alignment.center,
+      padding: const EdgeInsets.symmetric(
+        horizontal: 18.0,
+        vertical: 24.0,
+      ),
+      decoration: BoxDecoration(
+        color: AppColors.bgColor2,
+        borderRadius: BorderRadius.circular(
+          30.0,
+        ),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black54,
+            spreadRadius: 4.0,
+            blurRadius: 4.5,
+            offset: Offset(
+              3.0,
+              4.5,
             ),
-            width: 450.0,
-            child: Column(
-              children: [
-                Image.asset(
-                  AppAssets.github,
-                  width: 50.0,
-                  height: 50.0,
-                  color: AppColors.themeColor,
-                ),
-                Constants.sizedBox(
-                  height: 30.0,
-                ),
-                Text(
-                  'App Development',
-                  style: AppTextStyles.montserratStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                Constants.sizedBox(
-                  height: 12.0,
-                ),
-                Text(
-                  'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document ',
-                  style: AppTextStyles.normalStayle(),
-                ),
-                Constants.sizedBox(
-                  height: 12.0,
-                ),
-                AppButtons.buildMaterialButton(
-                  onTap: () {},
-                  buttonName: 'Read More',
-                ),
-              ],
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Image.asset(
+            asset,
+            width: 50.0,
+            height: 50.0,
+            color: AppColors.themeColor,
+          ),
+          Constants.sizedBox(
+            height: 30.0,
+          ),
+          Text(
+            title,
+            style: AppTextStyles.montserratStyle(
+              color: Colors.white,
             ),
+          ),
+          Constants.sizedBox(
+            height: 12.0,
+          ),
+          Text(
+            'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document ',
+            style: AppTextStyles.normalStayle(),
+          ),
+          Constants.sizedBox(
+            height: 20.0,
+          ),
+          AppButtons.buildMaterialButton(
+            onTap: () {},
+            buttonName: 'Read More',
           ),
         ],
       ),
