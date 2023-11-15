98814ef48b25e6f6f56957fe933f33e8bdaed739
mcalisadeghi <ali.ke970@gmail.com>
2023-11-16 02:04:01 +0330

    add images

--- lib/screen/my_portfolio.dart
+++ lib/screen/my_portfolio.dart
@@ -1,17 +1,33 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/globals/app_assets.dart';
 import 'package:flutter_application_1/globals/app_colors.dart';
 import 'package:flutter_application_1/globals/app_styles.dart';
 import 'package:flutter_application_1/globals/constants.dart';
 
-class MyPortfolio extends StatelessWidget {
+class MyPortfolio extends StatefulWidget {
   const MyPortfolio({super.key});
 
+  @override
+  State<MyPortfolio> createState() => _MyPortfolioState();
+}
+
+class _MyPortfolioState extends State<MyPortfolio> {
+  List images = <String>[
+    AppAssets.facebook,
+    AppAssets.facebook,
+    AppAssets.facebook,
+    AppAssets.facebook,
+    AppAssets.facebook,
+    AppAssets.facebook,
+    AppAssets.facebook,
+    AppAssets.facebook,
+  ];
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return Container(
-      alignment: Alignment.center,
+      // alignment: Alignment.center,
       padding: const EdgeInsets.symmetric(
         vertical: 30.0,
         horizontal: 12.0,
@@ -20,6 +36,7 @@ class MyPortfolio extends StatelessWidget {
       height: size.height,
       color: AppColors.bgColor2,
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
           FadeInDown(
             duration: const Duration(
@@ -46,6 +63,23 @@ class MyPortfolio extends StatelessWidget {
           Constants.sizedBox(
             height: 40.0,
           ),
+          GridView.builder(
+            itemCount: images.length,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisExtent: 200,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              var image = images[index];
+              return Image(
+                image: AssetImage(
+                  image,
+                ),
+              );
+            },
+          )
         ],
       ),
     );
